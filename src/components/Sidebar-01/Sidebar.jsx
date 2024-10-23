9956e1a671e59b3208b92f1e59b2a3201eb1edb0
Okki Dwi <64502893+okkidwi@users.noreply.github.com>
2024-10-24 01:14:49 +0700

    Update Sidebar.jsx

--- src/components/Sidebar-01/Sidebar.jsx
+++ src/components/Sidebar-01/Sidebar.jsx
@@ -27,9 +27,25 @@ function Sidebar() {
                   <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
                 </svg>
 
-                <p class="links-texts">Game Hub</p>
+                <p class="links-texts">Home</p>
             </li>
         </A>
+          
+
+
+
+      <A href="/catalog" link="" aria-current="page">
+          <li id="link-catalog">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book">
+                  <path d="M4 4h16v16H4z" />
+                  <path d="M4 4v16h16V4" />
+              </svg>
+            
+              <p className="links-texts">Catalog</p>
+            
+          </li>
+      </A>
+          
 
 
 
