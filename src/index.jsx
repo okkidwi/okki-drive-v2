c64e0210fd8d8c7b0417276485e2538d7ff4f799
Okki Dwi <64502893+okkidwi@users.noreply.github.com>
2024-10-24 10:05:53 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { I18nProvider } from "solid-i18next";
+import { I18nProvider } from "i18next";
 import i18n from "./i18n";
 import "./styles.css";
 import App from "./App";
