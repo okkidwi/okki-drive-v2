0cc6b43254404db5f6504e6ec60829e387c04a5a
Okki Dwi <64502893+okkidwi@users.noreply.github.com>
2024-10-24 10:12:43 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,32 +4,29 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [solid(),
+  plugins: [
+    solid(),
     viteStaticCopy({
-        targets: [
-          {
-            src: './splashscreen.html',  
-            dest: '.'                     
-          },
-          {
-            src: './src-tauri/icons/Square310x310Logo.png', 
-            dest: '.'  
-          }
-        ]
-      })
+      targets: [
+        {
+          src: './splashscreen.html',  
+          dest: '.'                     
+        },
+        {
+          src: './src-tauri/icons/Square310x310Logo.png', 
+          dest: '.'  
+        }
+      ]
+    })
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+  clearScreen: false, // prevent vite from obscuring rust errors
   server: {
-    port: 1420,
+    port: 1420, // tauri expects a fixed port, fail if that port is not available
     strictPort: true,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: ["**/src-tauri/**"], // tell vite to ignore watching `src-tauri`
     }
   },
   build: {
@@ -38,8 +35,4 @@ export default defineConfig(async () => ({
       external: ['i18next'], // Externalize i18next
     },
   },
-}));
-    target: 'esnext'
-  },
-  
 }));
