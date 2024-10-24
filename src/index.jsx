8cc488f8944e6a23628c67cbc37129150d264c4b
Okki Dwi <64502893+okkidwi@users.noreply.github.com>
2024-10-24 11:18:57 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { I18nProvider } from "i18next";
-import i18n from "./i18n";
 import "./styles.css";
 import App from "./App";
 
-render(
-  () => (
-    <I18nProvider i18n={i18n}>
-      <App />
-    </I18nProvider>
-  ),
-  document.getElementById("root")
-);
+render(() => <App />, document.getElementById("root"));
